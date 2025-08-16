43188923c61e946547c155db6ec3a157b24ad935
Mitch Naylor <mitchnaylor94@gmail.com>
2025-08-16 10:32:49 -0400

    add missing className in img tag

--- src/App.tsx
+++ src/App.tsx
@@ -117,7 +117,7 @@ function App() {
           rel="noopener noreferrer"
           className="github-link"
         >
-          <img src="./img/github-mark.svg" alt="GitHub logo" width="25" height="25" />
+          <img src="./img/github-mark.svg" alt="GitHub logo" width="25" height="25" className="github-icon" />
           View on GitHub
         </a>
       </footer>
