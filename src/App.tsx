22836e24ba0cf0630244bad5690575d2d50769ea
Mitch Naylor <mitchnaylor94@gmail.com>
2025-08-22 21:28:03 -0400

    add and use basic json data from old app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useMemo, lazy, Suspense } from 'react';
-import climbingAreasData from '../data/sample-climbing-areas.json';
+import climbingAreasData from '../data/climbing-areas-v1.json';
 import { ClimbingAreaData, ForecastResponse, HourlyForecastResponse } from './weatherData/weatherApi';
 import { useWeatherData, ForecastType } from './hooks/useWeatherData';
 import MultiSelect from './components/MultiSelect';
